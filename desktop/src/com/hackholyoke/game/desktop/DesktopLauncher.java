41e30c3a8426614a4876696d379ac30c979961dd
Sage Mahannah <mahan22s@mtholyoke.edu>
2021-11-13 11:31:55 -0500

    initial commit

---
+++ desktop/src/com/hackholyoke/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hackholyoke.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hackholyoke.game.HackHolyoke;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HackHolyoke(), config);
+	}
+}
